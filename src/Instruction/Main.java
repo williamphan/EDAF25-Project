1e35798e2438b2608c00ab8bd69a5de8d4a6d24d
Marcus Karlsson <agerroofficial@gmail.com>
2015-04-15 10:29:30 +0200

    m

--- src/Instruction/Main.java
+++ src/Instruction/Main.java
@@ -1,10 +1,16 @@
 package Instruction;
+package Computer;
+package Data;
 
 /**
  * Created by will on 2015-02-20.
  */
 public class Main {
     public static void main(String[] args) {
-
+    	Program factorial = new Factorial();
+    	System.out.println(factorial);
+    	Computer computer = new Computer(new LongMemory(1024));
+    	computer.load(factorial);
+    	computer.run();
     }
 }
