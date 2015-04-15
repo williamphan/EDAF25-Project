138644d3303a847420008bf84724ed66cd26ade3
OskarJermakowicz <oskar.jermakowicz@gmail.com>
2015-04-15 09:22:20 +0200

    Test2

--- src/Computer/Computer.java
+++ src/Computer/Computer.java
@@ -1,10 +1,27 @@
 package Computer;
 
+import Data.Memory;
+import Instruction.Instruction;
+
 /**
  * Created by will on 2015-02-20.
  */
 public class Computer {
-	int a = 0;
- //????
-
+	int counter = 0;
+	Program program;
+	Memory memory;
+	
+	public Computer(Memory memory) {
+		this.memory = memory;
+	}
+	
+	public void load(Program program) {
+		this.program = program;
+	}
+	
+	public void run() {
+		while (counter > -1) {
+			counter = program.get(counter).run(memory, counter);
+		}
+	}
 }
