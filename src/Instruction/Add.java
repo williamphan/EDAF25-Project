811fc91b83f58b47c71e351b339649bfe75ea886
OskarJermakowicz <oskar.jermakowicz@gmail.com>
2015-04-15 10:27:20 +0200

    Update Add.java

--- src/Instruction/Add.java
+++ src/Instruction/Add.java
@@ -8,19 +8,20 @@ import Data.Word;
  * Created by will on 2015-02-20.
  */
 public class Add extends Expression {
+	private Word w1;
+	private Word w2;
 
+	public Add(Data data1, Data data2, Address address) {
+		super(data1, data2, address);
+	}
 
-    public Add(Data data1, Data data2, Address address) {
-        super(data1, data2, address);
-    }
+	protected void op(Word w1, Word w2, Address address) {
+		this.w1 = w1;
+		this.w2 = w2;
+		// TODO: The add operation...
+	}
 
-    @Override
-    protected void op(Word w1, Word w2, Address address) {
-        //TODO
-    }
-
-    public String toString() {
-        //TODO
-        return null;
-    }
+	public String toString() {
+		return "ADD " + w1.toString() + " " + w2.toString() + " " + address.toString();
+	}
 }
