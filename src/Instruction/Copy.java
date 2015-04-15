8ccb0db2023cba4373ffc2f29051c7efbc09d846
OskarJermakowicz <oskar.jermakowicz@gmail.com>
2015-04-15 10:43:34 +0200

    Update Copy.java

--- src/Instruction/Copy.java
+++ src/Instruction/Copy.java
@@ -24,7 +24,6 @@ public class Copy implements Instruction {
     }
 
     public String toString(){
-        //TODO
-        return null;
+        return "CPY " + data.toString() + " " + address.toString();
     }
 }
