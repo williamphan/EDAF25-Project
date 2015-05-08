c5b697f22d47f5bf2e943dea8052a30a991f1917
Marcus Karlsson <agerroofficial@gmail.com>
2015-05-08 11:34:42 +0200

    min senaste

--- XL/src/gui/menu/ClearMenuItem.java
+++ XL/src/gui/menu/ClearMenuItem.java
@@ -1,16 +1,27 @@
 package gui.menu;
 
+import gui.CurrentSlot;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
 
+import Model.Sheet;
+
 class ClearMenuItem extends JMenuItem implements ActionListener {
-    public ClearMenuItem() {
-        super("Clear");
+   
+	private Sheet sheet;
+	private CurrentSlot currentSlot;
+	
+	public ClearMenuItem() {
+    	super("Clear");
+    	this.sheet=sheet;
+    	this.currentSlot=currentSlot;
         addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+        //sheet.removeSlot(currentSlot.getName());
     }
 }
