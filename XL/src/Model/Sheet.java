12dfd11d931ea7c74fb5883ba070fcec1ed8c06e
OskarJermakowicz <oskar.jermakowicz@gmail.com>
2015-05-08 09:58:11 +0200

    Small changes to Sheet

--- XL/src/Model/Sheet.java
+++ XL/src/Model/Sheet.java
@@ -1,34 +1,50 @@
 package Model;
 
-
 import expr.Environment;
 
 import java.util.HashMap;
 import java.util.Observable;
 
+import util.XLException;
+
 public class Sheet extends Observable implements Environment {
-    private HashMap<String, Slot> map;
+	private HashMap<String, Slot> map;
 
-    public Sheet() {
-        map = new HashMap<String, Slot>();
-    }
+	public Sheet() {
+		map = new HashMap<String, Slot>();
+	}
 
-    public boolean addSlot(String key, Slot value) {
-        return false;
-    }
+	/**
+	 * @return True if slot was added, false otherwise.
+	 */
+	public boolean addSlot(String key, Slot value) {
+		if (checkCircular(key, value))
+			return false;
 
-    public boolean removeSlot(String key) {
-        return false;
-    }
+		map.put(key, value);
+		// Notify?
 
-    private boolean checkCircular() {
-        return false;
-    }
+		return true;
+	}
 
-    //Plats för fler metoder
+	public boolean removeSlot(String key) {
+		return false;
+	}
 
-    @Override
-    public double value(String name) {
-        return 0;
-    }
+	/**
+	 * @return True if circular dependency is detected, false otherwise.
+	 */
+	private boolean checkCircular(String key, Slot value) {
+		return false;
+	}
+
+	// Plats för fler metoder
+
+	@Override
+	public double value(String name) {
+		if (map.get(name) == null) 
+			throw new XLException(name + " does not exist in the sheet.");
+		
+		return map.get(name).value(this);
+	}
 }
