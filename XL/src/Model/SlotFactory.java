9fee4d884e8438d82e2645a8dcc06c3a3312d9b2
Marcus Karlsson <agerroofficial@gmail.com>
2015-05-18 10:41:29 +0200

    SlotFactory, med tillägg att # inte kommer med i cellen

--- XL/src/Model/SlotFactory.java
+++ XL/src/Model/SlotFactory.java
@@ -5,7 +5,8 @@ import expr.ExprParser;
 public class SlotFactory {
 	public static Slot create(String text) {
 		if (text.length() > 0 && text.charAt(0) == '#') {
-			return new TextSlot(text);
+			String newText = text.substring(1);
+			return new TextSlot(newText);
 		}
 
 		ExprParser parser = new ExprParser();
