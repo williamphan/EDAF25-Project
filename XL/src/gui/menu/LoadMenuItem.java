359658ffa75268c50b82d1a4a248ff2e367e7c26
Nikolai Sneckenborg <nikolai@simbalsyd.se>
2015-05-18 09:17:29 +0200

    Ändrade LoadMenu (visste inte om att XLBufferedReader fanns när jag skrev i den innan)

--- XL/src/gui/menu/LoadMenuItem.java
+++ XL/src/gui/menu/LoadMenuItem.java
@@ -3,12 +3,11 @@ package gui.menu;
 import gui.StatusLabel;
 import gui.XL;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 import javax.swing.JFileChooser;
+
+import util.XLBufferedReader;
 
 class LoadMenuItem extends OpenMenuItem {
  
@@ -18,20 +17,9 @@ class LoadMenuItem extends OpenMenuItem {
 
     protected void action(String path) throws FileNotFoundException {
         // TODO
-    	FileReader fr = new FileReader(path);
-    	BufferedReader br = new BufferedReader(fr);
-    	String line;
-    	int current = 0;
-    	try{
-    		while((line = br.readLine()) != null){
-    			///////
-    			
-    			current++;
-    		}
-    	}
-    	catch(IOException ex){
-    		System.err.println(ex.getMessage());
-    	}
+    	XLBufferedReader buffer = new XLBufferedReader(path);
+    	buffer.load(xl.);
+    	buffer.close();
     }
 
     protected int openDialog(JFileChooser fileChooser) {
